fe6b8749bf7090d1a68bebc556a47821288b35ce
Gia Thuan Vu <vugiathuan123@gmail.com>
2021-10-03 21:07:45 -0700

    Completed part 3

---
+++ turnin/tvu115_lab2_part3.c
@@ -0,0 +1,21 @@
+/*	Author: thuanvu
+ *  Partner(s) Name: none
+ *	Lab Section:
+ *	Assignment: Lab 2 Exercise 1
+ *	Exercise Description: Intro to AVR roolchain
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    while(1){
+      PORTC = 4 - ((PINA & 0x08 >> 3) + (PINA & 0x04 >> 2) + (PINA & 0x02 >> 1) + (PINA & 0x01));
+      PORTC = PORTC ? (0x01 << 7) : PORTC; // if PORTC == 0, lot is full, just set PC7 else set to num of avail spots
+    }
+    return 1;
+}
