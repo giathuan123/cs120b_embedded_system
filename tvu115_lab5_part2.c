9fc195862818d0c49a48b2485201ce02aecb7dd8
Gia Thuan Vu <vugiathuan123@gmail.com>
2021-10-17 21:34:57 -0700

    Completed part 2

--- tvu115_lab5_part2.c
+++ tvu115_lab5_part2.c
@@ -76,6 +76,7 @@ int main(void) {
     buttonA0 = released;
     buttonA1 = released;
     while (1) {
+      PORTB = (PORTB &0xfc) +  (GetBit(PINA, 1) << 1) + (GetBit(PINA, 0))
       tick();
       PORTC = cnt;
     }
