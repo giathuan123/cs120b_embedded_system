ce6fc1db265ec9af09dbd68edf5185d9e4bdd349
Gia Thuan Vu <vugiathuan123@gmail.com>
2021-10-22 16:07:03 -0700

    Completed part 1

---
+++ tvu115_lab6_part1.c
@@ -0,0 +1,51 @@
+/*	Author: thuanvu
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab 6  Exercise 1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *      Demo Link: 
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+#include <timer.h>
+enum synchSM_state{start, on} state = start;
+unsigned char lights = 0x00;
+void tick(){
+  switch(state){
+    case(start):
+      state = on;
+      break;
+    case(on):
+      break;
+  }
+  switch(state){
+    case(start):
+      lights = 0x01;
+      break;
+    case(on):
+      lights = lights << 1;
+      if(lights == 0x08 || lights == 0x00)
+        lights = 0x01;
+  }
+}
+
+int main(){
+  DDRB = 0xFF;
+  PORTB = 0x00;
+  TimerSet(1000);
+  TimerOn();
+  while(1){
+    tick();
+    PORTB = lights;
+    while(!TimerFlag);
+    TimerFlag = 0;
+  }
+}
+
+
